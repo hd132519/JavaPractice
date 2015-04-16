ae8027f4a6024dab7dcedc23c47b7aae0055a7bb
박병건 <@PBG>
2015-04-16 12:26:06 +0900

    카2

--- JavaPractice/src/org/dimigo/oop/CarTest.java
+++ JavaPractice/src/org/dimigo/oop/CarTest.java
@@ -4,10 +4,11 @@ public class CarTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Car car = new Car();
-		Car car2 = new Car();
-		Car car3 = new Car();
+		Car2 car = new Car2("현대자동차","제네시스","검정색", 225, 50000000);
+		Car2 car2 = new Car2("기아자동차", "K7", "흰색", 246, 40000000);
+		Car2 car3 = new Car2("삼성자동차", "SM7", "회색", 200, 38000000);
 		
+		/*
 		car.setCompany("현대자동차");
 		car.setModel("제네시스");
 		car.setColor("검정색");
@@ -25,6 +26,9 @@ public class CarTest {
 		car3.setColor("회색");
 		car3.setMaxSpeed(200);
 		car3.setPrice(38000000);
+		*/
+		
+		
 		
 		System.out.println("<<자동차 목록>>");
 		System.out.printf("제조사명 : %s\n모델명 : %s\n색상 : %s\n최대속도 : %dkm\n가격 : %,d원\n",

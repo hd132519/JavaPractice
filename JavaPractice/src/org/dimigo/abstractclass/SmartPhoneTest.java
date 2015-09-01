22124735317fc52153a6d31be3c4dcdcef7863e8
박병건 <@PBG>
2015-09-01 10:15:57 +0900

    abstract SmartPhone

---
+++ JavaPractice/src/org/dimigo/abstractclass/SmartPhoneTest.java
@@ -0,0 +1,24 @@
+package org.dimigo.abstractclass;
+
+public class SmartPhoneTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		IPhone iph = new IPhone("iPhone 6", "Apple", 700000);
+		Galaxy gal = new Galaxy("Galaxy S6 Edge", "Samsung", 650000);
+		
+		System.out.println(iph);
+		iph.turnOn();
+		iph.useSpecialFunction();
+		iph.pay();
+		iph.turnOff();
+		
+		System.out.println(gal);
+		gal.turnOn();
+		gal.useSpecialFunction();
+		gal.pay();
+		gal.turnOff();
+		
+	}
+
+}
